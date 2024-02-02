0dbc49cc1e8a46c0e73610098846e92658eaf5b4
kfujita <kfujita@student.42tokyo.jp>
2024-02-02 21:55:34 +0900

    Reactプロジェクトを作成

---
+++ srcs/requirements/react/files/app/src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <>
+      <h1>{"Hello World!"}</h1>
+      <p>{"It is a React App!"}</p>
+    </>
+  );
+};
+
+export default App;
